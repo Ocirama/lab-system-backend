d8439fa55727deda8706b5794449823a2c5b5091
Justas <justinas.pedisius@gmail.com>
2019-11-26 08:24:25 +0200

    l

---
+++ src/main/java/lt/ocirama/labsystembackend/model/QualityControlEntity.java
@@ -0,0 +1,54 @@
+package lt.ocirama.labsystembackend.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+    @Entity
+    @Table(name = "quality_control_log")
+    public class QualityControlEntity extends AbstractEntity {
+
+        @Column(name = "test_type", length = 50, nullable = false)
+        private String testType;
+
+        @Column(name = "quality_tray_id", length = 50, nullable = false)
+        private String qualityTrayId;
+
+        @Column(name = "quality_tray_weight_before")
+        private double qualityTrayWeightBefore;
+
+        @Column(name = "quality_tray_weight_after")
+        private double qualityTrayWeightAfter;
+
+        public String getTestType() {
+            return testType;
+        }
+
+        public void setTestType(String testType) {
+            this.testType = testType;
+        }
+
+        public String getQualityTrayId() {
+            return qualityTrayId;
+        }
+
+        public void setQualityTrayId(String qualityTrayId) {
+            this.qualityTrayId = qualityTrayId;
+        }
+
+        public double getQualityTrayWeightBefore() {
+            return qualityTrayWeightBefore;
+        }
+
+        public void setQualityTrayWeightBefore(double qualityTrayWeightBefore) {
+            this.qualityTrayWeightBefore = qualityTrayWeightBefore;
+        }
+
+        public double getQualityTrayWeightAfter() {
+            return qualityTrayWeightAfter;
+        }
+
+        public void setQualityTrayWeightAfter(double qualityTrayWeightAfter) {
+            this.qualityTrayWeightAfter = qualityTrayWeightAfter;
+        }
+    }
---
+++ src/main/java/lt/ocirama/labsystembackend/repositories/QualityControlRepository.java
@@ -0,0 +1,118 @@
+package lt.ocirama.labsystembackend.repositories;
+
+import lt.ocirama.labsystembackend.model.ReferenceTrayEntity;
+import lt.ocirama.labsystembackend.services.FileControllerService;
+import lt.ocirama.labsystembackend.services.UserInputService;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.hibernate.Session;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Query;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.time.LocalDate;
+
+public class QualityControlRepository {
+    /*
+
+    private final EntityManagerFactory entityManagerFactory;
+
+    public QualityControlRepository(EntityManagerFactory entityManagerFactory) {
+        this.entityManagerFactory = entityManagerFactory;
+    }
+
+    public void ReferenceTrayLogGenerate() {
+        EntityManager em = entityManagerFactory.createEntityManager();
+        EntityTransaction transaction = em.getTransaction();
+        transaction.begin();
+        try {
+            System.out.println("Fiksuokite padėklo numerį ir svorį");
+            String padeklas = UserInputService.NumberInput();
+            ReferenceTrayEntity rte = new ReferenceTrayEntity();
+            rte.setReferenceTrayId(padeklas);
+
+            Double trayWeight = FileControllerService.sverimoPrograma("Off");
+            rte.setReferenceTrayWeightBefore(trayWeight);
+            em.persist(rte);
+            transaction.commit();
+            ReferenceTrayExcelUpdate(rte, 1);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void ReferenceTrayLogSecondGenerate() {
+        EntityManager em = entityManagerFactory.createEntityManager();
+        EntityTransaction transaction = em.getTransaction();
+
+        try {
+            String padeklas;
+            transaction.begin();
+            System.out.println("Fiskuokite padėklo numerį ir svorį:");
+            padeklas = UserInputService.NumberInput();
+            Session session = em.unwrap(Session.class);
+            Query query = session.createQuery("from ReferenceTrayEntity  where referenceTrayId=:padeklas");
+            query.setParameter("padeklas", padeklas);
+            ReferenceTrayEntity rte = (ReferenceTrayEntity) query.getSingleResult();
+            System.out.println("Sverkite padėklą po džiovinimo: ");
+            Double trayWeight = FileControllerService.sverimoPrograma("Off");
+            rte.setReferenceTrayWeightAfter(trayWeight);
+            em.persist(rte);
+            ReferenceTrayExcelUpdate(rte, 2);
+            transaction.commit();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void ReferenceTrayExcelUpdate(ReferenceTrayEntity rte, int sverimoNumeris) {
+        XSSFSheet sheet;
+        XSSFWorkbook workbook;
+        String path = "C:\\Users\\lei12\\Desktop\\Output\\" + LocalDate.now() + " (VisumineDregme).xlsx";
+        File file = new File(path);
+        try {
+            if (file.exists()) {
+                FileInputStream fsip = new FileInputStream(path);
+                workbook = new XSSFWorkbook(fsip);
+                if (workbook.getSheet("PamatinisPadeklas") == null) {
+                    sheet = workbook.createSheet("PamatinisPadeklas");
+                } else
+                    sheet = workbook.getSheet("PamatinisPadeklas");
+            } else {
+                workbook = new XSSFWorkbook();
+                sheet = workbook.createSheet("PamatinisPadeklas");
+            }
+            Row rowhead = sheet.createRow(0);
+            rowhead.createCell(0).setCellValue("Pamatinio padėklo Nr.");
+            rowhead.createCell(1).setCellValue("Pamatinio padėklo svoris PRIEŠ");
+            rowhead.createCell(2).setCellValue("Pamatinio padėklo svoris P0");
+            rowhead.createCell(3).setCellValue("Data");
+
+            if (sverimoNumeris == 1) {
+                Row row1 = sheet.createRow(sheet.getLastRowNum() + 1);
+                row1.createCell(0).setCellValue(rte.getReferenceTrayId());
+                row1.createCell(1).setCellValue(rte.getReferenceTrayWeightBefore());
+                row1.createCell(3).setCellValue(String.valueOf(LocalDate.now()));
+            }
+            if (sverimoNumeris == 2) {
+                Row row = sheet.getRow(FileControllerService.findRow(workbook, rte.getReferenceTrayId()));
+                row.createCell(2).setCellValue(rte.getReferenceTrayWeightAfter());
+            }
+            FileOutputStream fileOut = new FileOutputStream(path);
+            workbook.write(fileOut);
+            fileOut.flush();
+            fileOut.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}*/
+}
